201fd807bdec7c760de60f52cf92553a34221cd3
Monson Shao <holymonson@gmail.com>
2016-03-18 23:32:00 +0800

    let svgo handle dataURL
    Directly call decodeURIComponent may throw 'URIError: URI malformed' if the
    string is unencoded but contains '%'. Just let svgo handle the pre-check and
    decode/encode.
    
    update svgo version to 0.6.2
    
    remove lib/url

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import postcss from 'postcss';
 import valueParser from 'postcss-value-parser';
 import SVGO from 'svgo';
 import isSvg from 'is-svg';
-import {encode, decode} from './lib/url';
+import {encodeSVGDatauri, decodeSVGDatauri} from 'svgo/lib/svgo/tools';
 
-const dataURI = /data:image\/svg\+xml(;(charset=)?utf-8)?,/;
+const dataURI = /data:image\/svg\+xml/;
 
 function minifyPromise (svgo, decl, opts) {
     const promises = [];
@@ -13,28 +13,25 @@ function minifyPromise (svgo, decl, opts) {
         if (node.type !== 'function' || node.value !== 'url' || !node.nodes.length) {
             return;
         }
-        let value = node.nodes[0].value;
 
-        let decodedUri = decode(value);
-        let isUriEncoded = decodedUri !== value;
-        if (isUriEncoded) {
-            value = decodedUri;
-        }
-        if (opts.encode !== undefined) {
-            isUriEncoded = opts.encode;
-        }
-
-        if (!dataURI.test(value) || !isSvg(value)) {
+        const value = node.nodes[0].value;
+        const data = decodeSVGDatauri(value);
+        if (!isSvg(data)) {
             return;
         }
+
+        let isUriEncoded = opts.encode;
+        if (isUriEncoded === undefined) {
+            isUriEncoded = value.indexOf(data) === -1;
+        }
+
         promises.push(new Promise((resolve, reject) => {
-            svgo.optimize(value.replace(dataURI, ''), result => {
+            svgo.optimize(data, result => {
                 if (result.error) {
                     return reject(`Error parsing SVG: ${result.error}`);
                 }
                 node.before = node.after = '';
-                let data = isUriEncoded ? encode(result.data) : result.data;
-                node.nodes[0].value = 'data:image/svg+xml;charset=utf-8,' + data;
+                node.nodes[0].value = encodeSVGDatauri(result.data, isUriEncoded ? 'enc' : 'unenc');
                 node.nodes[0].quote = isUriEncoded ? '"' : '\'';
                 node.nodes[0].type = 'string';
                 resolve();
